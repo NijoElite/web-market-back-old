c1625735750acc457c7c32d135bd831a0e523f00
Eugene Malchenko <nijoelite@gmail.com>
2019-09-21 19:07:19 +0300

    Add api error handling

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,19 @@ const router = express.Router();
 router.use('/reg', require('./registration')); 
 router.use('/auth', require('./auth'));
 
+// Errors
+router.use(function(err, req, res, next) {
+  if (err.name === 'ValidationError') {
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key) {
+        errors[key] = err.errors[key].message;
+        return errors;
+      }, {}),
+    });
+  }
+
+  next(err);
+});
+
+
 module.exports = router;
